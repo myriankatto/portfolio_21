0901647378ddc93746c5edd66898de4e9776cbf2
Myrian Katto <myriankatto@gmail.com>
2021-05-17 15:49:07 +0200

    BundleAnalyzerPlugin

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+//...
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  //...
+  plugins: [
+    //...
+    new BundleAnalyzerPlugin(),
+  ],
+};
