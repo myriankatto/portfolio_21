5992b6c49fadba0d846f51e4838407f6652805b8
Myrian Katto <myriankatto@gmail.com>
2021-05-17 16:33:58 +0200

    improving perfomance

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
 
 import { ThemeProvider } from 'styled-components';
